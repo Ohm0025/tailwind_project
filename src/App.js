9ed1838b894237c5f3a15d3bd6f0b4e4d95f9b40
Ohm0025 <toyohm0025@gmail.com>
2023-11-28 13:39:32 +0700

    add responsive

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       className="min-h-screen py-40"
       style={{ backgroundImage: "linear-gradient(115deg,#9f7aea,#fee2fe)" }}>
       <div className="container mx-auto">
-        <div className="flex w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
           <div
-            className="w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
+            className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
             style={{
               backgroundImage:
                 "url('https://img.freepik.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg?size=626&ext=jpg&ga=GA1.1.2116175301.1701043200&semt=ais')",
@@ -25,7 +25,7 @@ function App() {
               </p>
             </div>
           </div>
-          <div className="w-1/2 py-16 px-12">
+          <div className="w-full lg:w-1/2 py-16 px-12">
             <h2 className="text-3xl mb-4">Register</h2>
             <p className="mb-4">
               Create your account. It's free and only take a minute
